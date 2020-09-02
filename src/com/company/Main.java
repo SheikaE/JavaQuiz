eaebee64732218d26ec784ee9a3381d29ea50cd4
Sheika Etienne <sheikaetienne@Sheikas-MacBook-Pro.local>
2020-09-02 16:07:25 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int years = 4;
+	String graduate = ("I am excited to graduate this year!");
+	float gpa = 3.2f;
+	boolean yes = true;
+
+	System.out.println("I have been at Howard for " + years + " years");
+	System.out.println("I am a senior. " + graduate);
+	System.out.println("My GPA is " + gpa);
+	System.out.println("It is " + yes + " that I enjoy coding.");
+
+
+
+    }
+}
